89abecd3b859f833b8fe8a9587b4c47dbb029be9
Jeffrey Onochie <jeff.ict@gmail.com>
2021-06-27 06:40:16 +0100

    adds delete user, create,update token handlers

--- index.js
+++ index.js
@@ -112,5 +112,6 @@ const unifiedServer = (req, res) => {
 // Define requests router
 const router = {
     'ping': handlers.ping,
-    'users': handlers.users
+    'users': handlers.users,
+    'tokens': handlers.tokens
 };
